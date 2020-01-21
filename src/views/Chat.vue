cfe707bf470c5df91aef3e9a8da092b1ead0d49d
Jason <jason71708@gmail.com>
2020-01-22 03:44:09 +0800

    發布版本

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -143,8 +143,8 @@ import JMessage from '@/components/smallCP/JMessage';
 import JUserInOutMessage from '@/components/smallCP/JUserInOutMessage';
 
 import io from "socket.io-client";
-const socket = io("http://localhost:3000/");
-// const socket = io("https://f2e-week7-2019.herokuapp.com:3000");
+// const socket = io("http://localhost:3000/");
+const socket = io("https://f2e-week7-2019.herokuapp.com:5000");
 
 export default {
   name: 'chat',
