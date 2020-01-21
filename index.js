c534e92e0bd1437341ff1cdab3fa9f9bae186553
Jason <jason71708@gmail.com>
2020-01-22 03:11:55 +0800

    測試發布

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const path = require('path'); //new
 var express = require('express');
+const cool = require('cool-ascii-faces')//heroku
 const serveStatic = require('serve-static');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000; //new
+var port = process.env.PORT || 5000; //new
 
 server.listen(port);
 // WARNING: app.listen(80) will NOT work here!
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));  //new
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})//new
 
 io.on('connection', function (socket) {
   socket.on('userInOut', function (data) {
